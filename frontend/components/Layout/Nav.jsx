c242bfa716a2feb0590332fe4b1f922680af44ab
Benwin <benwingeorge@gmail.com>
2025-02-01 18:24:35 -0500

    Minor UI changes

--- frontend/components/Layout/Nav.jsx
+++ frontend/components/Layout/Nav.jsx
@@ -106,7 +106,7 @@ const Nav = () => {
           justifyContent="space-between"
         >
           <Link href="/">
-            <a className={styles.logo}>Next.js MongoDB App</a>
+            <a className={styles.logo}>Food Tracker</a>
           </Link>
           <Container>
             {user ? (
